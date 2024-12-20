b6ce0e5c5ff36347380141029dfddc0ea1bf7926
Andrii Shybunko <andriyshybunko@gmail.com>
2024-12-20 16:07:48 +0200

    Added constants and updated a little main

---
+++ lib/constants/text_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:random_cat_facts/constants/app_colors.dart';
+
+class AppTextStyles {
+  static TextStyle comfortaaRegular = TextStyle(
+    fontFamily: 'Comfortaa',
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+    fontSize: 16.sp,
+  );
+}
