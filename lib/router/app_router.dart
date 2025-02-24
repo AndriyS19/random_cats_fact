529d2857c9482ddc6babb9734a58aa0bb906cef7
Andrii Shybunko <andriyshybunko@gmail.com>
2025-02-24 16:08:56 +0200

    Add app router

---
+++ lib/router/app_router.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:random_cat_facts/nav_bar/nav_bar.dart';
+import 'package:random_cat_facts/screens/history/history_page.dart';
+import 'package:random_cat_facts/screens/home/home.page.dart';
+import 'package:random_cat_facts/screens/settings/settings_page.dart';
+
+final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> _shellNavigatorKey = GlobalKey<NavigatorState>();
+
+final goRouter = GoRouter(
+  navigatorKey: _rootNavigatorKey,
+  initialLocation: '/home',
+  routes: [
+    ShellRoute(
+      navigatorKey: _shellNavigatorKey,
+      builder: (context, state, child) {
+        return CustomNavigationBar(child: child);
+      },
+      routes: [
+        GoRoute(
+          path: '/history',
+          pageBuilder: (context, state) => NoTransitionPage(
+            key: state.pageKey,
+            child: const HistoryPage(),
+          ),
+        ),
+        GoRoute(
+          path: '/home',
+          pageBuilder: (context, state) => NoTransitionPage(
+            key: state.pageKey,
+            child: const HomePage(),
+          ),
+        ),
+        GoRoute(
+          path: '/settings',
+          pageBuilder: (context, state) => NoTransitionPage(
+            key: state.pageKey,
+            child: const SettingsPage(),
+          ),
+        ),
+      ],
+    ),
+  ],
+);
